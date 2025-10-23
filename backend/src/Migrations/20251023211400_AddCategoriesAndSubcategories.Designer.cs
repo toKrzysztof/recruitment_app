0df69731c49159736d678e872b26e758a43388af
Krzysztof Torończak <k.toronczak.dev@gmail.com>
2025-10-23 23:14:13 +0200

    feature: add migrations

--- backend/src/Migrations/20251023211400_AddCategoriesAndSubcategories.Designer.cs
+++ backend/src/Migrations/20251023211400_AddCategoriesAndSubcategories.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using RecruitmentApp.Features.Contacts.Data;
@@ -11,9 +12,11 @@ using RecruitmentApp.Features.Contacts.Data;
 namespace RecruitmentApp.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251023211400_AddCategoriesAndSubcategories")]
+    partial class AddCategoriesAndSubcategories
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ backend/src/Migrations/20251023211400_AddCategoriesAndSubcategories.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace RecruitmentApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCategoriesAndSubcategories : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Contacts",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SubcategoryId",
+                table: "Contacts",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Subcategory",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    CategoryId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Subcategory", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Subcategory_Category_CategoryId",
+                        column: x => x.CategoryId,
+                        principalTable: "Category",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_CategoryId",
+                table: "Contacts",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_Email",
+                table: "Contacts",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Contacts_SubcategoryId",
+                table: "Contacts",
+                column: "SubcategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Category_Name",
+                table: "Category",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subcategory_CategoryId",
+                table: "Subcategory",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Subcategory_Name",
+                table: "Subcategory",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Category_CategoryId",
+                table: "Contacts",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Contacts_Subcategory_SubcategoryId",
+                table: "Contacts",
+                column: "SubcategoryId",
+                principalTable: "Subcategory",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Category_CategoryId",
+                table: "Contacts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Contacts_Subcategory_SubcategoryId",
+                table: "Contacts");
+
+            migrationBuilder.DropTable(
+                name: "Subcategory");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_CategoryId",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_Email",
+                table: "Contacts");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Contacts_SubcategoryId",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Contacts");
+
+            migrationBuilder.DropColumn(
+                name: "SubcategoryId",
+                table: "Contacts");
+        }
+    }
+}
